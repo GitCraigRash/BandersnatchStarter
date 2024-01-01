1a5c4fd1d3fd796a7d56d1b07841067a4d0ed356
Craig Rash Jr <90971454+GitCraigRash@users.noreply.github.com>
2024-01-01 10:42:53 -0500

    Update machine.py

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,44 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import joblib
+from app.data import Database
+
 class Machine:
 
     def __init__(self, df):
-        pass
+        df.loc[:,"Rarity"] = df["Rarity"].apply(lambda x: int(str(x)[-1]))
+        n = len(df[df['Rarity'] == 5])*10
+        df = df.groupby('Rarity').apply(lambda x: x.head(n) if x['Rarity'].iloc[0] != 5 else x).reset_index(drop=True)
+        target = df["Rarity"]
+        
+        columns_to_drop= ["Rarity"]
+        df = df.drop(columns=columns_to_drop)
+        
+        scaler = StandardScaler().fit(df)
+        features = scaler.transform(df)
+
+        self.name = "Random Forest Classifier"
+        self.model = RandomForestClassifier(n_estimators=230, random_state=32)
+        self.model.fit(features,target)
+
+        
 
     def __call__(self, feature_basis):
-        pass
+        prediction = self.model.predict(feature_basis)
+        prediction_prob = self.model.predict_proba(feature_basis)
 
+        prob = max(prediction_prob[0]).round(4)
+        return prediction[0],"{:.2%}".format(prob)
     def save(self, filepath):
-        pass
+        joblib.dump(self.model,'model.joblib')
 
     @staticmethod
     def open(filepath):
-        pass
+        return joblib.load('model.joblib')
 
     def info(self):
-        pass
+        db = Database()
+        df = db.dataframe
+        return db.collection.find_one({"Timestamp"}), db.name
